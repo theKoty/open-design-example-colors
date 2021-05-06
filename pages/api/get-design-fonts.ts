4642c327b8f7fdc69ad328488ea82ad0c14eca53
Marek Černý <marekcerny@avocode.com>
2021-05-06 15:29:26 +0200

    feat: add get design fonts EP

---
+++ pages/api/get-design-fonts.ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { createSdk } from '../../src/utils/sdk'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const designId = req.query.designId || process.env.EXAMPLE_DESIGN_ID
+
+  if (!designId || typeof designId !== 'string') {
+    return res.status(404)
+  }
+
+  const sdk = createSdk()
+  const design = await sdk.fetchDesignById(designId)
+  const fonts = await design.getFonts()
+
+  sdk.destroy
+
+  return res.status(200).json(fonts)
+}
