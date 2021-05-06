e378639e809bf6da3163b4298cc91315989636c7
Marek Černý <marekcerny@avocode.com>
2021-05-06 15:30:42 +0200

    feat: add render designs EP

---
+++ pages/api/render-design.ts
@@ -0,0 +1,40 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import fs from 'fs'
+import { createSdk } from '../../src/utils/sdk'
+
+const PUBLIC_FOLDER = './public/'
+
+function createDesignFolder(designId: string) {
+  const folderPath = `${PUBLIC_FOLDER}${designId}`
+
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath)
+  }
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const designId = req.query.designId || process.env.EXAMPLE_DESIGN_ID
+
+  if (!designId || typeof designId !== 'string') {
+    return res.status(404)
+  }
+
+  const sdk = createSdk()
+  const design = await sdk.fetchDesignById(designId)
+  const artboards = design.getArtboards()
+
+  createDesignFolder(designId)
+
+  const filePaths = []
+  await Promise.all(
+    artboards.map((artboard) => {
+      const filePath = `${designId}/${artboard.id}.png`
+      filePaths.push(filePath)
+      return artboard.renderToFile(`${PUBLIC_FOLDER}${filePath}`)
+    }),
+  )
+
+  sdk.destroy()
+
+  return res.status(200).json(filePaths)
+}
